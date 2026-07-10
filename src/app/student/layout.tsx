303b937ba23527ccc37dcdff20dc077536cdc6cb
Nitin Dube <nitindube227@gmail.com>
2026-07-10 23:45:25 +0530

    add

--- src/app/student/layout.tsx
+++ src/app/student/layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { StudentPortalProvider } from "@/components/student-portal/student-portal-ui";
-import "@/components/student-portal/student-portal.css";
 
 export default function StudentPortalLayout({ children }: { children: React.ReactNode }) {
   return (
